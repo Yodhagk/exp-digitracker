9a5d53a5bf658009d141b754f5ed2cc37b64ebbd
Yodhagk <gkotapatti@gmail.com>
2021-04-29 21:52:01 +0530

    changed from PDO to Mysql connection code to get connnected DB

--- config.php
+++ config.php
@@ -1,17 +1,14 @@
 <?php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'Citi123');
-define('DB_NAME', 'simple-lamp');
+$servername = "localhost";
+$username = "root";
+$password = "Citi123";
 
-/* Attempt to connect to MySQL database */
-try{
-    $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
-    // Set the PDO error mode to exception
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e){
-    die("ERROR: Could not connect. " . $e->getMessage());
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+ die("Connection failed: " . mysqli_connect_error());
 }
+echo "Connected successfully";
 ?>
